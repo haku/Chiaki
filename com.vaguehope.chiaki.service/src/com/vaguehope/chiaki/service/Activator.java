09ec3920a8391721fd6a35e6a1236cb58328d4f8
はく <haku@vaguehope.com>
2012-02-22 23:47:16 +0000

    Configure logger via fragment bundle.

--- com.vaguehope.chiaki.service/src/com/vaguehope/chiaki/service/Activator.java
+++ com.vaguehope.chiaki.service/src/com/vaguehope/chiaki/service/Activator.java
@@ -6,6 +6,8 @@ import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * TODO get CamelContext from Blueprint.
@@ -14,6 +16,8 @@ import org.osgi.framework.BundleContext;
 public class Activator implements BundleActivator {
 
 	private static final String BROKER_URL = "tcp://localhost:61616";
+	
+	private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
 	
 	private CamelContext camelContext;
 	private Pinger pinger;
@@ -26,6 +30,8 @@ public class Activator implements BundleActivator {
 
 		this.pinger = new Pinger(this.camelContext.createProducerTemplate());
 		this.pinger.start();
+		
+		LOG.info("Service started.");
 	}
 
 	@Override
@@ -35,6 +41,8 @@ public class Activator implements BundleActivator {
 
 		this.camelContext.stop();
 		this.camelContext = null;
+		
+		LOG.info("Service stopped.");
 	}
 
 }
--- com.vaguehope.chiaki.service/src/com/vaguehope/chiaki/service/Pinger.java
+++ com.vaguehope.chiaki.service/src/com/vaguehope/chiaki/service/Pinger.java
@@ -4,11 +4,17 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import org.apache.camel.ProducerTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Pinger extends TimerTask {
 
+	private static final String ENDPOINT_URL = "activemq:topic:example.foo";
+
 	private static final long DELAY = 10L * 1000L; // 10 seconds.
 
+	private static final Logger LOG = LoggerFactory.getLogger(Pinger.class);
+	
 	private final Timer timer = new Timer();
 
 	private final ProducerTemplate producerTemplate;
@@ -19,15 +25,18 @@ public class Pinger extends TimerTask {
 
 	public void start () {
 		this.timer.scheduleAtFixedRate(this, DELAY, DELAY);
+		LOG.info("Pinger scheduled.");
 	}
 
 	public void dispose () {
 		this.timer.cancel();
+		LOG.info("Pinger disposed.");
 	}
 
 	@Override
 	public void run () {
-		this.producerTemplate.sendBody("activemq:topic:example.foo", "desu~");
+		this.producerTemplate.sendBody(ENDPOINT_URL, "desu~");
+		LOG.debug("Pinger send to {}.", ENDPOINT_URL);
 	}
 
 }
